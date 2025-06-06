5bcf93d1afdcaceb22c10cd930624bb0955482a5
Camelia <cameliaafolabi@gmail.com>
2025-06-06 16:38:21 +0100

    fixed db error

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,7 +1,12 @@
 import { drizzle } from 'drizzle-orm/neon-http';
-import {neon} from "@neondatabase/serverless";
+import { neon } from '@neondatabase/serverless';
+import * as schema from './schema';
 
-import * as schema from "./schema"
+const dbUrl = process.env.DATABASE_URL;
 
-const sql = neon(process.env.DATABASE_URL);
-export const db = drizzle(sql, {schema});
+if (!dbUrl) {
+  throw new Error('DATABASE_URL is not defined');
+}
+
+const sql = neon(dbUrl);
+export const db = drizzle(sql, { schema });
